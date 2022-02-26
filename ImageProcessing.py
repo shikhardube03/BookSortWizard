3d1dcf31d688bf59f5f7351c203c8700827fa228
nsunkad <nsunkad2@illinois.edu>
2022-02-26 15:03:46 -0600

    image processing, sorting books into lists

---
+++ ImageProcessing.py
@@ -0,0 +1,32 @@
+import pytesseract as tess
+from pytesseract import image_to_string
+from PIL import Image
+import cv2
+
+# tesseract --help-extra or tesseract --help-psm to find info about the segmentation modes
+
+img = Image.open("Label1.png")
+img = img.convert('L') # convert image to black and white
+
+data = tess.image_to_string(img, lang = 'eng',config='--psm 1')
+
+# all_lines = data.splitlines()
+print(data)
+
+all_lines = data.splitlines()
+
+def group_lines_into_books(sequence, separator):
+    book = []
+    for elem in sequence:
+        if elem == separator:
+            yield book
+            book = []
+        book.append(elem)
+    yield book
+
+result = list(group_lines_into_books(all_lines, ''))
+print(result)
+
+    
+
+#note: . sometimes gets read as -
---
+++ book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, row1, row2, row3, row4):
+        self.row1 = row1
+        self.row2 = row2
+        self.row3 = row3
+        self.row4 = row4
+    
